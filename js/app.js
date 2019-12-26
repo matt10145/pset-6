44fde19a3deab23716f5e1551c35fc7010488c82
matth101 <mhui@ucvts.org>
2019-12-25 22:23:03 -0500

    implement adding-task function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 window.onload = function() {
-    
+    document.getElementById("addButton").onclick = addTask;
 
-    
 }
 
-const addtask = function() {
+let tasks = [];
 
+const addTask = function(event) {
+    event.preventDefault();
+    input = document.getElementById("addedTask").value;
+    if (input == "" || input == null) {
+        alert("You haven't entered a task.");
+        return;
+    } 
+    else {
+        // intentionally empty
+    }
+    tasks.push(input);
+    const list = document.getElementById("ul");
+    let lineBreak = document.createElement("br");
+    let newItem = document.createElement("li");
+    newItem.innerHTML = input;
+    list.append(lineBreak);
+    list.append(newItem);
 }
 
 const completeTask = function() {
@@ -13,7 +29,7 @@ const completeTask = function() {
 }
 
 const incompleteTask = function() {
-    
+
 }
 
 const removeTask = function() {
